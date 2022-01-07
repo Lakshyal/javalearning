1bbc032a2ee3b038951ad17e212a868db4d05665
Lakshya Kumar Tiwari <60040943+Lakshyal@users.noreply.github.com>
2022-01-07 09:38:18 +0530

    Create First.java

---
+++ Programs/First.java
@@ -0,0 +1,12 @@
+//Develop a prorgam to display "Lakshya World!" on console.
+//Step#1 Open a notepad (press--Start->type Notepad->)
+//Step#2 Type below program 
+class First {
+   public static void main (String[] args) {
+   
+      System.out.println("Lakshya World!")
+   }
+}
+//Step#3 Save the above program with the name "First.java" in "D:\01CJ\01JB"(you must create those two folders
+
+//Step#4 Comple the above program by using jaavac tool using command prompt/console.
