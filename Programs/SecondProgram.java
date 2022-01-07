e95f537d2ce0c47c518997783d7d3551f9c990c5
Lakshya Kumar Tiwari <60040943+Lakshyal@users.noreply.github.com>
2022-01-07 10:05:06 +0530

    Create SecondProgram.java

---
+++ Programs/SecondProgram.java
@@ -0,0 +1,9 @@
+//Develop a program to display your name on console?
+
+class SecondProgram {
+
+   public static void main (String[] args) {
+   
+     System.out.println("Lakshya Kumar Tiwari");
+   }
+}
