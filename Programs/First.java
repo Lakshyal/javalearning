587001d539f44b8dce6fda8bbf40d3c9bf29e464
Lakshya Kumar Tiwari <60040943+Lakshyal@users.noreply.github.com>
2022-01-07 10:00:24 +0530

    Update First.java

--- Programs/First.java
+++ Programs/First.java
@@ -10,3 +10,14 @@ class First {
 //Step#3 Save the above program with the name "First.java" in "D:\01CJ\01JB"(you must create those two folders
 
 //Step#4 Comple the above program by using jaavac tool using command prompt/console.
+   //1. Open command prompt (press Start--> type cmmd--> press Enter)
+   //  C:\Users\dell>
+   //2. change drive to D:
+   //C:\Users\dell>D:(Type D: and press enter key)
+   //3. Change directory to "01JB"
+   // D:\>cd 01CJ\01JB 
+   //4. Compile "First.java" by using javac tool
+   //  D:\01CJ\01JB>javac First.java----(generates First.class file  ie..bytecoded file)
+   // Execute the avove program by using java tool 
+   // Run the .class file by using java tool 
+   // 01CJ/01JB>java First.class
