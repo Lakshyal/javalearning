9a2e47777afdcd59c853d23eda55b25d7e92e05b
Lakshya Kumar Tiwari <60040943+Lakshyal@users.noreply.github.com>
2022-01-07 10:15:21 +0530

    Create NameStar.java

---
+++ Programs/NameStar.java
@@ -0,0 +1,16 @@
+//Develop a program to display your name in stars?
+/*     
+     *******
+   * Lakshya *
+     *******
+     */
+class NameStar {
+
+  public static void main (String[] agrs)  {
+      System.out.println("  *******");
+      System.out.println("* Lakshya *");
+      System.out.println("  *******");
+  }
+
+}
+
